3af02a8b6fde6ef0255609e4cdc72584783d624a
Marcel Tanner <marcel.tanner@uzh.ch>
2019-01-18 01:46:50 +0100

    header change (user icon), new comp growing textarea

---
+++ GrowingTextarea.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="growing-textarea">
+        <textarea ref="textarea" :placeholder="placeholder" v-model="inputValue" :style="styleObject"></textarea>
+        <span ref="text">Joghurt</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "GrowingTextarea",
+        data() {
+            return {
+                inputValue: '',
+                firstHeight: null,
+                lineHeight: null,
+                padding: null,
+                numberOfLines: 1,
+                styleObject: null
+            }
+        },
+        props: {
+            placeholder: String,
+            value: String,
+            fat: Boolean
+        },
+        mounted() {
+            this.firstHeight = this.$refs.textarea.offsetHeight;
+            this.padding = this.firstHeight - this.$refs.text.offsetHeight;
+            this.lineHeight = this.firstHeight - this.padding;
+
+            if( this.fat ) {
+                this.styleObject = {'font-weight':'700'};
+            }
+        },
+        watch: {
+            inputValue() {
+                this.$emit('input', this.inputValue);
+
+                this.$refs.textarea.style.height = (this.lineHeight + this.padding)+'px';
+            },
+            value() {
+                this.inputValue = this.value;
+
+                this.numberOfLines = Math.floor( this.$refs.textarea.scrollHeight / this.lineHeight );
+                this.$refs.textarea.style.height = (this.numberOfLines * this.lineHeight + this.padding)+'px';
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .growing-textarea {
+        position: relative;
+        span {
+            position: absolute;
+            left: 0;
+            top: 0;
+            visibility: hidden;
+            display: block;
+            line-height: 1.5;
+        }
+    }
+</style>
