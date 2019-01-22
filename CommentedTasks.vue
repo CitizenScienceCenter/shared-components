93665521c3cea44a4ae5be48f89a1ce579f714a6
Marcel Tanner <marcel.tanner@uzh.ch>
2019-01-22 10:20:19 +0100

    comments wip

--- CommentedTasks.vue
+++ CommentedTasks.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="comments">
         <ul>
-            <li v-for="task in tasks">
-                <router-link tag="button" :to="'/challenge/'+task.id" class="button button-primary">{{ task.id }}</router-link>
+            <li v-for="(task, index) in tasks">
+                <router-link tag="button" :to="'/challenge/'+tasks[index].id" class="button button-primary">{{ tasks[index].id }}</router-link>
             </li>
         </ul>
     </div>
@@ -26,7 +26,9 @@
                 tasks: state => state.c3s.task.tasks,
                 taskMedia: state => state.c3s.task.media,
 
-                loading: state => state.c3s.settings.loading
+                loading: state => state.c3s.settings.loading,
+
+                activityId: state => state.consts.activityId
             })
         },
         mounted() {
@@ -34,71 +36,66 @@
         },
         methods: {
             loadCommentedTasks: function() {
-                console.log('load commented tasks');
-                console.log(this.sourceId);
+                //console.log('load commented tasks');
 
 
-                let taskQuery = {
+                let query = {
                     'select': {
                         'fields': [
+                            'tasks.id as task_id',
                             '*'
                         ],
                         'tables': [
                             'tasks'
                         ]
                     },
+                    "join": {
+                        "type": "LEFT",
+                        "conditions":{
+                            "from": {
+                                "table": "media",
+                                "field": "source_id"
+                            },
+                            "to": {
+                                "table": "tasks",
+                                "field": "id"
+                            }
+                        }
+                    },
                     'where': [
                         {
                             "field": 'activity_id',
                             'op': 'e',
-                            'val': this.sourceId
+                            'val': this.activityId
+                        },
+                        {
+                            'field': 'tasks.id',
+                            'op': 'i',
+                            'val': "(SELECT source_id FROM comments WHERE comments.source_id = tasks.id)",
+                            'join': 'a',
+                            'type': 'sql'
                         }
                     ]
                 };
 
+                this.$store.dispatch('c3s/media/getMedia', [query, undefined, 0]).then(res => {
 
-                this.$store.dispatch('c3s/task/getTasks', [taskQuery, 0]).then(tasks => {
+                    console.log('media & task_id loaded');
 
+                    let task_ids = [];
+                    let media = [];
+                    for( let i = 0; i < res.body.length; i++ ) {
+                        task_ids.push( { 'id': res.body[i].task_id } );
 
-                    console.log( 'tasks loaded');
-
-                    console.log( this.tasks );
-
-
-                    if ( this.tasks.length > 0 ) {
-
-
-                        /*
-                        const mediaQuery = {
-                            'select': {
-                                'fields': [
-                                    '*'
-                                ],
-                                'tables': [
-                                    'media'
-                                ]
-                            },
-                            'where': [
-                                {
-                                    "field": "source_id",
-                                    'op': 'e',
-                                    'val': this.tasks[0].id
-                                }
-                            ]
-                        };
-
-                        this.$store.dispatch('c3s/media/getMedia', [mediaQuery, 'c3s/task/SET_MEDIA', 0]).then(media => {
-
-                            console.log('media loaded');
-
-
-                            console.log( this.media );
-
-                        });
-                        */
-
+                        delete res.body[i].task_id;
+                        media.push( res.body[i] );
                     }
 
+                    this.$store.commit('c3s/task/SET_TASKS', task_ids );
+                    this.$store.commit('c3s/media/SET_MEDIA', media );
+
+
+                    //console.log( media );
 
                 });
 
--- Comments.vue
+++ Comments.vue
@@ -11,28 +11,38 @@
             <growing-textarea v-model="commentText" placeholder="What's on your mind?"></growing-textarea>
         </div>
 
-        <div class="button-group right-aligned  ">
-            <button :disabled="this.commentTitle === '' || this.commentText === ''" class="button button-primary" @click="newComment()">Comment</button>
+        <div class="button-group right-aligned">
+            <button v-if="withTitles" :disabled="commentTitle === '' || commentText === ''" class="button button-primary" @click="newComment()">Comment</button>
+            <button v-else :disabled="commentText === ''" class="button button-primary" @click="newComment()">Comment</button>
         </div>
 
-        <ul class="list">
-            <li v-for="comment in commentTree">
+        <ul class="comment-list">
+            <li v-for="(comment,index) in commentTree" :ref="'comment-'+index">
+
+                {{ comment[2] }}
                 <h3 class="subheading">{{ comment[0].content.title }}</h3>
                 <p>{{ comment[0].content.text }}</p>
-                <button v-if="comment[1].length > 0" class="button button-secondary">{{ comment[1].length }} Replies</button>
-                <span v-else>No Replies</span>
 
-                <div class="form-field">
-                    <label>Reply</label>
-                    <input type="text" v-model="replyText" />
+                <template v-if="opened.indexOf(index) > -1">
+                    <ul class="reply-list">
+                        <li v-for="reply in comment[1]">
+                            <p>{{ child.content.text }}</p>
+                        </li>
+                    </ul>
+                    <div class="form-field">
+                        <growing-textarea v-model="replyText" placeholder="Reply"></growing-textarea>
+                    </div>
+                    <div class="button-group right-aligned">
+                        <button :disabled="replyText.length === 0" class="button button-primary" @click="newComment(comment[0].id)">Reply</button>
+                    </div>
+                </template>
+
+                <div v-else class="button-group right-aligned">
+                    <button @click.prevent="opened.push(index)" class="button button-secondary button-secondary-naked">
+                        <template v-if="comment[1].length > 0">{{ comment[1].length }} Replies</template>
+                        <template v-else>Reply</template>
+                    </button>
                 </div>
-                <button class="button button-secondary" @click="newComment(comment[0].id)">Reply</button>
-
-                <ul class="children">
-                    <li v-for="child in comment[1]">
-                        <p>{{ child.content.text }}</p>
-                    </li>
-                </ul>
             </li>
         </ul>
     </div>
@@ -49,6 +59,8 @@
         components: {GrowingTextarea},
         data() {
             return {
+                commentTree: [],
+                opened: [],
                 commentTitle: '',
                 commentText: '',
                 replyText: ''
@@ -67,8 +79,6 @@
         watch: {
             sourceId: function() {
                 this.loadComments();
-            },
-            commentText() {
             }
         },
         computed: {
@@ -77,13 +87,63 @@
                 comments: state => state.c3s.comments.comments,
 
                 loading: state => state.c3s.settings.loading
-            }),
-            commentTree: function() {
-                let tree = [];
+            })
+        },
+        mounted: function() {
+            //console.log('comments mounted > load comments');
+            this.loadComments();
+        },
+        methods: {
+            loadComments: function() {
+                //console.log('load comments');
+                //console.log(this.sourceId);
+
+                let query = {
+                    'select': {
+                        'fields': [
+                            'users.username as username',
+                            '*'
+                        ],
+                        'tables': [
+                            'comments'
+                        ]
+                    },
+                    "join": {
+                        "type": "LEFT",
+                        "conditions":{
+                            "from": {
+                                "table": "users",
+                                "field": "id"
+                            },
+                            "to": {
+                                "table": "comments",
+                                "field": "user_id"
+                            }
+                        }
+                    },
+                    'where': [
+                        {
+                            "field": 'source_id',
+                            'op': 'e',
+                            'val': this.sourceId
+                        }
+                    ]
+                };
+
+
+                this.$store.dispatch('c3s/media/getMedia', [query, 'c3s/comments/SET_COMMENTS', 0] ).then(res => {
+
+                    //console.log('comments loaded');
+                    this.buildTree();
+
+                });
+            },
+            buildTree: function() {
+                this.commentTree = [];
                 for( let i = this.comments.length-1; i >= 0; i-- ) {
                     if( this.comments[i].parent === null ) {
                         //console.log( 'no p' );
-                        tree.unshift( [ this.comments[i], [] ] );
+                        this.commentTree.unshift( [ this.comments[i], [] ] );
                         //console.log( 'tree length: '+ tree.length );
                     }
                     else {
@@ -93,40 +153,24 @@
                         for( let j = 0; j < tree.length; j++ ) {
                             //console.log( 'check: ');
                             //console.log( tree[j][0].id );
-                            if( tree[j][0].id ===  this.comments[i].parent ) {
+                            if( this.commentTree[j][0].id ===  this.comments[i].parent ) {
                                 //console.log('found in '+ tree[j][0] )
-                                tree[j][1].unshift( this.comments[i] );
+                                this.commentTree[j][1].unshift( this.comments[i] );
                                 break;
                             }
                         }
                     }
                 }
-                return tree;
-            }
-        },
-        mounted: function() {
-            console.log('comments mounted > load comments');
-            this.loadComments();
-        },
-        methods: {
-            loadComments: function() {
-                //console.log('load comments');
-                //console.log(this.sourceId);
-                this.$store.dispatch('c3s/comments/getCommentsForID', [this.sourceId, 'c3s/comments/SET_COMMENTS', 0] ).then(comments => {
-
-                    console.log('comments loaded');
-
-                });
             },
             newComment: function(parentId) {
 
 
-                console.log('new comment');
+                //console.log('new comment');
 
                 let comment;
                 if( !parentId ) {
 
-                    console.log('no parent');
+                    //console.log('no parent');
 
                     comment = {
                         user_id: this.user.id,
@@ -145,8 +189,7 @@
                         source_id: this.sourceId,
                         parent: parentId,
                         content: {
-                            title: this.commentTitle,
-                            text: this.commentText
+                            text: this.replyText
                         }
                     };
                 }
@@ -155,13 +198,16 @@
 
                 this.$store.dispatch('c3s/comments/createComment', comment).then(res => {
 
-                    this.commentTitle = '';
-                    this.commentText = '';
-
-                    console.log('comment created > load comments');
+                    console.log(res);
                     this.loadComments();
 
                 });
+            },
+            expand( i ) {
+                console.log( this.commentTree[i][2] );
+                this.commentTree[i][2] = true;
+                console.log( this.commentTree[i][2] );
+                this.commentTree = this.commentTree;
             }
         }
     }
@@ -173,15 +219,44 @@
     @import '@/styles/theme.scss';
     @import '@/styles/shared/variables.scss';
 
+
     .comments {
         .form-field {
             display: block;
 
             margin-bottom: $spacing-3;
         }
-        .list {
-            margin-top: $spacing-4;
+        ul {
+            li {
+                padding-left: 0;
+                margin-left: 0;
+                &:before {
+                    display: none;
+                }
+            }
         }
+        .comment-list {
+            margin-top: $spacing-4;
+
+            .subheading, p {
+                margin-bottom: $spacing-2;
+            }
+        }
+    }
+
+    @media only screen and (min-width: $viewport-tablet-portrait) {
+
+        .comments {
+
+            .comment-list {
+                margin-top: $spacing-5;
+
+                .subheading, p {
+                    margin-bottom: $spacing-3;
+                }
+            }
+        }
+
     }
 
 </style>
