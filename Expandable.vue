9d092ee5d6433d1d64f596f7215ba2b52952e81b
Marcel Tanner <marcel.tanner@uzh.ch>
2019-04-17 12:19:33 +0200

    new expandable component (wip)

---
+++ Expandable.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="expandable reduced-bottom-margin" :class="{open:open}">
+        <div class="header">
+            <h3 @click="toggle">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M352.26,273l-136,136a23.9,23.9,0,0,1-33.9,0l-22.6-22.6a23.9,23.9,0,0,1,0-33.9l96.4-96.4-96.4-96.4a23.9,23.9,0,0,1,0-33.9l22.5-22.8a23.9,23.9,0,0,1,33.9,0l136,136A23.93,23.93,0,0,1,352.26,273Z"/></svg>
+                <slot name="header"></slot>
+            </h3>
+        </div>
+        <div class="content-box" :style="{height:contentBoxHeight+'px'}">
+            <div class="content" ref="content">
+                <slot name="content"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Expandable",
+        data() {
+            return {
+                open: false,
+                contentHeight: undefined
+            }
+        },
+        computed: {
+            contentBoxHeight() {
+                if( this.open ) {
+                    return this.contentHeight;
+                }
+                else {
+                    return 0;
+                }
+            }
+        },
+        methods: {
+            toggle() {
+                this.contentHeight = this.$refs.content.offsetHeight;
+                this.open = !this.open;
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+    @import '@/styles/theme.scss';
+    @import '@/styles/shared/variables.scss';
+
+    .expandable {
+        .header {
+            h3 {
+                font-size: $font-size-medium;
+                font-weight: 700;
+                color: $color-primary;
+                cursor: pointer;
+                position: relative;
+                padding-left: $spacing-3;
+
+                svg {
+                    position: absolute;
+                    top: calc( ( #{$font-size-medium} *1.25 - #{$font-size-normal} ) /2 );
+                    left: 0;
+                    width: $font-size-normal;
+                    height: $font-size-normal;
+                    fill: $color-primary;
+                }
+
+                &:visited {
+                    color: $color-primary;
+                }
+                &:active {
+                    color: $color-primary-shade-20;
+                }
+                @media (hover: hover) {
+                    &:hover {
+                        color: $color-primary-shade-20;
+                    }
+                }
+            }
+        }
+        .content-box {
+            overflow: hidden;
+            transition: height $transition-duration-short $transition-timing-function;
+            .content {
+                padding: $spacing-2 0;
+                padding-left: $spacing-3;
+            }
+        }
+
+        &.open {
+            .content-box {
+
+            }
+        }
+    }
+
+</style>
