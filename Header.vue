c72bfbd4cc407ffa0f07524a930805f76a8fad6e
Marcel Tanner <marcel.tanner@uzh.ch>
2019-05-28 11:46:19 +0200

    nothing

--- Header.vue
+++ Header.vue
@@ -174,7 +174,6 @@ export default {
                   });
                 }
             }
-            console.log( visibleRoutes );
             return visibleRoutes;
         },
         currentUser: {
