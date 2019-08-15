ac21e20449c6361657d0dd8da47b706aec2c1fde
Marcel Tanner <marcel.tanner@uzh.ch>
2019-08-15 14:19:17 +0200

    new stats section

---
+++ SectionStats.vue
@@ -0,0 +1,60 @@
+<template>
+    <app-content-section :color=" color ? color : false" class="content-section-condensed stats-section">
+        <div class="content-wrapper">
+            <div class="row row-centered row-wrapping">
+
+                <div v-if="submissionCount" class="col col-wrapping col-large-2 scroll-effect centered">
+                    <label>Number of Submissions</label>
+                    <span class="value">{{submissionCount}}</span>
+                </div>
+
+                <div v-if="userCount" class="col col-wrapping col-large-2 scroll-effect centered">
+                    <label>Number of Users</label>
+                    <span class="value">{{userCount}}</span>
+                </div>
+
+                <div v-if="taskCount" class="col col-wrapping col-large-2 scroll-effect centered">
+                    <label>Number of Tasks</label>
+                    <span class="value">{{taskCount}}</span>
+                </div>
+
+            </div>
+        </div>
+    </app-content-section>
+</template>
+
+<script>
+
+    import ContentSection from '@/components/shared/ContentSection.vue';
+
+    export default {
+        name: "SectionStats",
+        components: {
+            'app-content-section': ContentSection
+        },
+        props: {
+            color: String,
+
+            submissionCount: Number,
+            userCount: Number,
+            taskCount: Number
+        }
+    }
+</script>
+
+<style lang="scss">
+
+    @import '@/styles/theme.scss';
+    @import '@/styles/shared/variables.scss';
+
+    .stats-section {
+        label {
+            margin-right: $spacing-2;
+        }
+        .value {
+            font-size: $font-size-medium;
+            font-weight: 700;
+        }
+    }
+
+</style>
