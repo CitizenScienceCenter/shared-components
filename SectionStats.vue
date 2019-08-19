940efcb33b4a27506c9857aa94d85291cf6cb126
Marcel Tanner <marcel.tanner@uzh.ch>
2019-08-19 19:50:13 +0200

    stats section changes

--- SectionStats.vue
+++ SectionStats.vue
@@ -1,20 +1,43 @@
+<i18n>
+    {
+
+
+    "de": {
+
+
+    "number-of-submissions": "Anzahl<br>Antworten",
+    "number-of-users": "Anzahl<br>Benutzer",
+    "number-of-tasks": "Anzahl<br>Aufgaben"
+
+    },
+    "en": {
+
+    "number-of-submissions": "Number<br>of Submissions",
+    "number-of-users": "Number<br>of Users",
+    "number-of-tasks": "Number<br>of Tasks"
+
+    }
+
+    }
+</i18n>
+
 <template>
     <app-content-section :color=" color ? color : false" class="content-section-condensed stats-section">
         <div class="content-wrapper">
             <div class="row row-centered row-wrapping">
 
                 <div v-show="submissionCount" class="col col-wrapping col-large-3 col-xlarge-2 scroll-effect centered">
-                    <label>Number of Submissions</label>
+                    <label v-html="$t('number-of-submissions')"></label>
                     <span class="value">{{submissionCount}}</span>
                 </div>
 
                 <div v-show="userCount" class="col col-wrapping col-large-3 col-xlarge-2 scroll-effect scroll-effect-delayed-1 centered">
-                    <label>Number of Users</label>
+                    <label v-html="$t('number-of-users')"></label>
                     <span class="value">{{userCount}}</span>
                 </div>
 
                 <div v-show="taskCount" class="col col-wrapping col-large-3 col-xlarge-2 scroll-effect scroll-effect-delayed-2 centered">
-                    <label>Number of Tasks</label>
+                    <label v-html="$t('number-of-tasks')"></label>
                     <span class="value">{{taskCount}}</span>
                 </div>
 
@@ -50,9 +73,12 @@
     .stats-section {
         label {
             margin-right: $spacing-2;
+            display: inline-block;
+            text-align: right;
+            line-height: 1.1;
         }
         .value {
-            font-size: $font-size-large;
+            font-size: $font-size-xlarge;
             font-weight: 700;
         }
     }
