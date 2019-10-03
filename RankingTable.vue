9024af44456d0235c8b103c3ddf03482bebc99a1
Marcel Tanner <marcel.tanner@uzh.ch>
2019-10-03 17:09:05 +0200

    ranking table + stats as subsection

---
+++ RankingTable.vue
@@ -0,0 +1,53 @@
+<i18n>
+    {
+    "de": {
+
+    "label-username": "Benutzername",
+    "label-score": "Anzahl Antworten",
+
+    "anonymous-username": "Anonymer Benutzer"
+
+    },
+    "en": {
+
+    "label-username": "Username",
+    "label-score": "Number of Submissions",
+
+    "anonymous-username": "Anonymous User"
+
+    }
+    }
+</i18n>
+
+<template>
+    <table class="ranking">
+        <tr>
+            <th></th>
+            <th>{{ $t('label-username') }}</th>
+            <th>{{ $t('label-score') }}</th>
+        </tr>
+        <tr v-for="rankingEntry in ranking" v-if="limit === undefined || rankingEntry.rank <= limit" :key="rankingEntry.username">
+            <td>
+                <template v-if="!rankingEntry.sameAsPrevious">{{rankingEntry.rank}}.</template>
+            </td>
+            <td><b>
+                <template v-if="!rankingEntry.username.search('_anon')">{{ $t('anonymous-username') }}</template>
+                <template v-else>{{rankingEntry.username}}</template>
+            </b></td>
+            <td>{{rankingEntry.subs}}</td>
+        </tr>
+    </table>
+</template>
+
+<script>
+    export default {
+        name: "RankingTable",
+        props: {
+            ranking: Array,
+            limit: {
+                type: Number,
+                default: undefined
+            }
+        }
+    }
+</script>
--- SectionStats.vue
+++ SectionStats.vue
@@ -5,16 +5,24 @@
     "de": {
 
 
-    "number-of-submissions": "Anzahl<br>Antworten",
-    "number-of-users": "Anzahl<br>Benutzer",
-    "number-of-tasks": "Anzahl<br>Aufgaben"
+    "label-number-of-submissions": "Gesamte Anzahl<br>Antworten",
+    "label-number-of-users": "Anzahl<br>Benutzer",
+    "label-number-of-tasks": "Anzahl<br>Aufgaben",
+
+    "label-number-of-my-submissions": "Anzahl Ihrer<br>Antworten",
+    "label-my-progress": "Ihr<br>Fortschritt",
+    "label-my-rank": "Ihr<br>Rang"
 
     },
     "en": {
 
-    "number-of-submissions": "Number of<br>Submissions",
-    "number-of-users": "Number<br>of Users",
-    "number-of-tasks": "Number<br>of Tasks"
+    "label-number-of-submissions": "Total Number of<br>Submissions",
+    "label-number-of-users": "Number<br>of Users",
+    "label-number-of-tasks": "Number<br>of Tasks",
+
+    "label-number-of-my-submissions": "Number of my<br>Submissions",
+    "label-my-progress": "My<br>Progress",
+    "label-my-rank": "My<br>Rank"
 
     }
 
@@ -27,18 +35,42 @@
             <div class="row row-centered row-wrapping">
 
                 <div v-show="submissionCount !== undefined" class="col col-wrapping col-large-3 col-xlarge-2 scroll-effect centered">
-                    <label v-html="$t('number-of-submissions')"></label>
+                    <label v-html="$t('label-number-of-submissions')"></label>
                     <span class="value">{{submissionCount}}</span>
                 </div>
 
                 <div v-show="userCount !== undefined" class="col col-wrapping col-large-3 col-xlarge-2 scroll-effect scroll-effect-delayed-1 centered">
-                    <label v-html="$t('number-of-users')"></label>
+                    <label v-html="$t('label-number-of-users')"></label>
                     <span class="value">{{userCount}}</span>
                 </div>
 
                 <div v-show="taskCount !== undefined" class="col col-wrapping col-large-3 col-xlarge-2 scroll-effect scroll-effect-delayed-2 centered">
-                    <label v-html="$t('number-of-tasks')"></label>
+                    <label v-html="$t('label-number-of-tasks')"></label>
                     <span class="value">{{taskCount}}</span>
+                </div>
+
+
+                <div v-show="mySubmissionCount !== undefined" class="col col-wrapping col-large-3 col-xlarge-2 scroll-effect centered">
+                    <label v-html="$t('label-number-of-my-submissions')"></label>
+                    <span class="value">{{mySubmissionCount}}</span>
+                </div>
+
+                <div v-show="myProgress !== undefined" class="col col-wrapping col-large-3 col-xlarge-2 scroll-effect scroll-effect-delayed-1 centered">
+                    <label v-html="$t('label-my-progress')"></label>
+                    <span class="value">{{myProgress}}%</span>
+                </div>
+
+                <div v-show="myRank !== undefined" class="col col-wrapping col-large-3 col-xlarge-2 scroll-effect scroll-effect-delayed-2 centered">
+                    <label v-html="$t('label-my-rank')"></label>
+                    <span class="value">{{myRank}}.</span>
+                </div>
+
+                <div v-show="ranking !== undefined" class="col col-wrapping col-large-6 scroll-effect">
+                    <h3 class="subheading centered">Top Contributors</h3>
+                    <ranking-table :ranking="ranking" :limit="3"></ranking-table>
+                    <div class="button-group centered">
+                        <router-link tag="button" to="/ranking/current" class="button button-secondary">Show Full Ranking</router-link>
+                    </div>
                 </div>
 
             </div>
@@ -49,10 +81,14 @@
 <script>
 
     import ContentSection from '@/components/shared/ContentSection.vue';
+    import Ranking from "../Ranking";
+    import RankingTable from "./RankingTable";
 
     export default {
         name: "SectionStats",
         components: {
+            RankingTable,
+            Ranking,
             'app-content-section': ContentSection
         },
         props: {
@@ -63,7 +99,13 @@
 
             submissionCount: Number,
             userCount: Number,
-            taskCount: Number
+            taskCount: Number,
+
+            mySubmissionCount: Number,
+            myProgress: Number,
+            myRank: Number,
+
+            ranking: Array
         }
     }
 </script>
