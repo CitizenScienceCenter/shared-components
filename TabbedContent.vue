24756adbca03c51f723c639a62fec621c0df1825
Marcel Tanner <marcel.tanner@uzh.ch>
2019-10-29 19:11:46 +0100

    stats your not my / tabbed content wip

---
+++ TabbedContent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="tabbed-content">
+        {{ tabs }}
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TabbedContent",
+        data() {
+            return {
+                tabs: [],
+                openTab: 0
+            }
+        },
+        mounted() {
+            for( let i=0; i<this.$children.length; i++ ) {
+                this.tabs.push( this.$children[i].$slots.title[0].text );
+            }
+            this.$children[0].active = true;
+        }
+    }
+</script>
+
+<style lang="scss">
+
+    .tabbed-content {
+        .tab {
+            display: none;
+            &.active {
+                display: block;
+             }
+        }
+    }
+
+</style>
